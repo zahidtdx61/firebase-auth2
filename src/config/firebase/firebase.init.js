209e90feff76831de306277f30c3549b40f268cb
Md. Zahidur Rahman <45038726+zahidtdx61@users.noreply.github.com>
2024-04-10 13:11:13 +0600

    update firebase config

--- src/config/firebase/firebase.init.js
+++ src/config/firebase/firebase.init.js
@@ -1,15 +1,17 @@
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: import.meta.VITE_apiKey,
-  authDomain: import.meta.VITE_authDomain,
-  projectId: import.meta.VITE_projectId,
-  storageBucket: import.meta.VITE_storageBucket,
-  messagingSenderId: import.meta.VITE_messagingSenderId,
-  appId: import.meta.VITE_appId,
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
 
-export default app;
+export default auth;
